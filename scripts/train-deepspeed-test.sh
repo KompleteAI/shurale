2fcf84c3e9413cbaede4044548a66a6ac0d41edb
Boris Zubarev <bobazooba@gmail.com>
2023-09-28 15:40:11 +0400

    update

--- scripts/train-deepspeed-test.sh
+++ scripts/train-deepspeed-test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 deepspeed --num_gpus=2 shurale/cli/train.py --use_gradient_checkpointing True --deepspeed_stage 2 --stabilize True \
-  --model_name_or_path mistralai/Mistral-7B-v0.1 --use_flash_attention_2 Flase \
+  --model_name_or_path mistralai/Mistral-7B-v0.1 --use_flash_attention_2 False \
   --load_in_4bit True --apply_lora True --raw_lora_target_modules all --per_device_train_batch_size 6 \
   --warmup_steps 100 --max_steps 500 --save_total_limit 0 --push_to_hub True --hub_model_id KompleteAI/Shurale7b-v1-LoRA-Test \
   --hub_private_repo True --report_to_wandb True --path_to_env_file ./.env
